ce58457abdca9d68fecc798db585ebe193a1f9e2
ayatsakalla <525asakalla@frhsd.com>
2026-09-06 21:19:54 -0400

    ok i think i did the form thing in the footer

---
+++ subscribe.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $to = "an@anetteanokhin.com";
+        $subject = "New Newsletter Signup";
+        $message = "New subscriber email: " . $email;
+        $headers = "From: noreply@anetteanokhin.com";
+
+        mail($to, $subject, $message, $headers);
+
+        header("Location: index.html?subscribed=success");
+        exit();
+    } else {
+        header("Location: index.html?subscribed=error");
+        exit();
+    }
+}
+?>
